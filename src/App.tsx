73ed096456045949344001ec42476fd4b27f0525
moregorenine <moregorenine@gmail.com>
2022-08-28 20:59:21 +0900

    state LoggedIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import {Welcome} from "./components/Welcome";
+import {LoggedIn} from "./components/state/LoggedIn";
 
 function App() {
     return (
         <div>
             <Welcome name={"jin"}/>
+            <LoggedIn></LoggedIn>
         </div>
     );
 }
